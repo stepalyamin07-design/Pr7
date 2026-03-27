a46d945c622d84017e98b247c63f6a9bed46e7a7
Степан <Stepan_lyamin07@mail.ru>
2026-03-27 11:20:42 +0300

    Задание 2(Галактика)

--- PR7_PiT/Program.cs
+++ PR7_PiT/Program.cs
@@ -1,15 +1,79 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace PR7_PiT
+
+namespace ConsoleApp_FirstApp
 {
-    internal class Program
+    class Program
     {
+        /// <summary>
+        /// Точка входа в приложение. Создаёт список галактик и выводит их на консоль.
+        /// </summary>
+        /// <param name="args">Аргументы командной строки (не используются).</param>
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Galaxy News!");
+            IterateThroughList();
+            Console.ReadKey();
         }
+
+        private static void IterateThroughList()
+        {
+            var theGalaxies = new List<Galaxy>
+        {
+            new Galaxy() { Name="Tadpole", MegaLightYears=400, GalaxyType=new GType('S')},
+            new Galaxy() { Name="Pinwheel", MegaLightYears=25, GalaxyType=new GType('S')},
+            new Galaxy() { Name="Cartwheel", MegaLightYears=500, GalaxyType=new GType('L')},
+            new Galaxy() { Name="Small Magellanic Cloud", MegaLightYears=.2, GalaxyType=new GType('I')},
+            new Galaxy() { Name="Andromeda", MegaLightYears=3, GalaxyType=new GType('S')},
+            new Galaxy() { Name="Maffei 1", MegaLightYears=11, GalaxyType=new GType('E')}
+        };
+
+            foreach (Galaxy theGalaxy in theGalaxies)
+            {
+                Console.WriteLine(theGalaxy.Name + "  " + theGalaxy.MegaLightYears + ",  " + theGalaxy.GalaxyType);
+            }
+        }
+    }
+    /// <summary>
+    /// Представляет галактику с именем и типом.
+    /// </summary>
+    public class Galaxy
+    {
+        /// <summary>
+        /// Название галактики.
+        /// </summary>
+        public string Name { get; set; }
+
+        public double MegaLightYears { get; set; }
+        public object GalaxyType { get; set; }
+
+    }
+
+    public class GType
+    {
+        public GType(char type)
+        {
+            switch (type)
+            {
+                case 'S':
+                    MyGType = Type.Spiral;
+                    break;
+                case 'E':
+                    MyGType = Type.Elliptical;
+                    break;
+                case 'l':
+                    MyGType = Type.Irregular;
+                    break;
+                case 'L':
+                    MyGType = Type.Lenticular;
+                    break;
+                default:
+                    break;
+            }
+        }
+        public object MyGType { get; set; }
+        private enum Type { Spiral, Elliptical, Irregular, Lenticular }
     }
 }
